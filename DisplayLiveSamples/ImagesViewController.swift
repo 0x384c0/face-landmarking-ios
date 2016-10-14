d9f69d994101c5e302743f1000f33e9ab749bdb0
Andrew <0x384c0@gmail.com>
2016-10-14 10:28:30 +0600

    added backgrounding

--- DisplayLiveSamples/ImagesViewController.swift
+++ DisplayLiveSamples/ImagesViewController.swift
@@ -9,29 +9,53 @@
 import UIKit
 
 class ImagesViewController: UIViewController {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBAction func back(sender: AnyObject) {
         print("dismissViewControllerAnimated")
         dismissViewControllerAnimated(true, completion: nil)
     }
     override func viewDidLoad() {
-        wrapper.prepare()
+        loading(true)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {[unowned self] in
+            self.wrapper.prepare()
+            self.loading(false)
+        }
+        
     }
     
     
     let wrapper = DlibWrapper()
     
     @IBAction func findFaces(sender: AnyObject) {
-        let
-        image = imageView.image!.CGImage,
-        size = imageView.image!.size
         
-        let imageBuffer: CVPixelBufferRef = Converters.pixelBufferFromCGImage(image!, withSize: size)!
-        wrapper.drawFaceLandMarksOnImageBuffer(imageBuffer)
+        loading(true)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {[unowned self] in
+            let
+            image = self.imageView.image!.CGImage,
+            size = self.imageView.image!.size
+            
+            let imageBuffer: CVPixelBufferRef = Converters.pixelBufferFromCGImage(image!, withSize: size)!
+            self.wrapper.drawFaceLandMarksOnImageBuffer(imageBuffer)
+            
+            dispatch_async(dispatch_get_main_queue()) {[unowned self] in
+                self.imageView.image = Converters.UIImageFromPixelBuffer(imageBuffer)
+                self.loading(false)
+            }
+        }
         
-        imageView.image = Converters.UIImageFromPixelBuffer(imageBuffer)
+        
     }
-   
+    
+    
+    func loading(isLoading:Bool) {
+        dispatch_async(dispatch_get_main_queue()) {[unowned self] in
+            if isLoading{
+                self.presentViewController(UIAlertController(title: "Processing...", message: nil, preferredStyle: .Alert), animated: true, completion: nil)
+            } else {
+                self.presentedViewController?.dismissViewControllerAnimated(true, completion: nil)
+            }
+        }
+    }
     
 }
